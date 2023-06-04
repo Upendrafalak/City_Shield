918bf0c0ad3eefd35949224c3136c24a32434c83
ManasLalwani <107605584+ManasLalwani@users.noreply.github.com>
2023-06-04 06:38:47 +0530

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,14 @@ import pandas as pd
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
-
+import qrcode
+from flask_cors import CORS
+import werkzeug
+import datetime
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 
@@ -17,33 +22,55 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
-# 3:-rape  2:-murder   0:-burglary  1:-fraud
-pklmodel = pickle.load(open("models/model_rank.pkl","rb"))
-@app.route("/assignofficer",methods=["POST"])
-def assignofficer():
-    data = request.get_json()
-    crime = data['crime']
-    # location = data['location']
-    money = data['money']
-    if crime == "rape":
-        crime_no = 3
-    elif crime == "murder":
-        crime_no = 2
-    elif crime == "burglary":
-        crime_no = 0
-    elif crime == "fraud":
-        crime_no = 1
-    designation_no = pklmodel.predict([[crime_no,money]]).reshape(-1,1).astype(int)
-    if designation_no == 0:
-        designation = ["ASP"]
-    elif designation_no == 1:
-        designation = ["DSP"]
-    elif designation_no == 2:
-        designation = ["Inspector"]
-    json_data = json.dumps(designation)
-    return json_data
-    
 
+def generate_qr_code_with_image_url_and_strings(image_url, strings):
+    # Combine the image URL and strings
+    combined_data = image_url + "\n" + "\n".join(strings)
+
+    # Create a QR code instance
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    # Add the combined data to the QR code
+    qr.add_data(combined_data)
+    qr.make(fit=True)
+
+    # Create an image from the QR code
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the image as a file
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    qr_image.save(f"./static/{timestamp}qr_code.png")
+
+@app.route('/secure-url', methods=['POST'])
+def handle_secure_url():
+    data = request.json
+    secure_url = data['url']
+    string1 = data['string1']
+    string2 = data['string2']
+    string3 = data['string3']
+    print(secure_url)
+    generate_qr_code_with_image_url_and_strings(secure_url,[string1,string2,string3])
+    return secure_url
+
+
+
+@app.route('/generate_qr_code', methods=['POST'])
+def generate_qr_code():
+    image_url = handle_secure_url
+    string1 = request.form['string1']
+    string2 = request.form['string2']
+    string3 = request.form['string3']
+
+    strings = [string1, string2, string3]
+
+    generate_qr_code_with_image_url_and_strings(image_url, strings)
+
+    return 'QR code generated successfully!'
 
 
 
